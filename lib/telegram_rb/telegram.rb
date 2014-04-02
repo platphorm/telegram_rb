6fb4fb332143b2e034252dc4f44851a5192de896
Jiren Patel <jiren@joshsoftware.com>
2014-04-02 16:33:06 +0530

    removed print statement

--- lib/telegram_rb/telegram.rb
+++ lib/telegram_rb/telegram.rb
@@ -1,9 +1,7 @@
 module Telegram
 
   def self.receive_message(message)
-    p message.inspect 
-    #p message.from_id.inspect
-    #Telegram.send_message(message.from_id, "From client: #{message.text}")
+    #p message.inspect 
   end
 
 end
